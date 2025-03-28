0ce8267ff4dd2fe1197076845e35e4856d7f2dd5
omea92 <thdjun92@naver.com>
2025-03-28 16:26:26 +0900

    상품 주문, 취소, 검색 서비스 실습

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -33,7 +33,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //검색 : JPA 동적쿼리 기능 JPQL 별도 공부필요...
+    //검색 : JPA 동적쿼리 기능 JPQL 별도 공부필요..., 실무에서는 잘안씀
     public List<Order> findAll(OrderSearch orderSearch) {
         String jpql = "select o From Order o join o.member m";
         boolean isFirstCondition = true;
