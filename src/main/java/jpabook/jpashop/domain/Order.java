cd3b0bc41d876c032b0debfa2bf5bbdae6f1aacb
omea92 <thdjun92@naver.com>
2025-03-28 15:48:10 +0900

    상품등록 도메인 비즈니스로직 실습

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,10 +17,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 @Entity
 @Table(name = "orders")
 @Getter
+@Setter
 public class Order {
 
     @Id
@@ -42,7 +45,7 @@ public class Order {
     private LocalDateTime orderDate; //자바 8이상부터 날짜처리는 해당 함수 지원권장
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+    private OrderStatus status;
 
     //연관관계 메소드
     public void setMember(Member member) {
@@ -58,5 +61,43 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //도메인주도 엔티티안에서 비즈니스로직 작성 : 객체지향적인 설계
+    //주문생성 메소드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //주문취소
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //전체 주문가격 조회
+    public int getTotalPrice() {
+        return orderItems.stream()
+                         .mapToInt(OrderItem::getTotalPrice)
+                         .sum();
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -30,4 +30,23 @@ public class OrderItem {
 
     private int orderPrice; //주문가격
     private int count; //주문수량
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count); //주문취소하면 수량원복
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
