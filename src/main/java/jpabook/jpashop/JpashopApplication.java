415c4fb76460d5ff109b8e99bb0535c0ece1e933
omea92 <thdjun92@naver.com>
2025-03-28 12:25:28 +0900

    gradle 스프링부트 라이브러리 주석 정리

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,6 +3,15 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+    spring-boot-starter-web
+    - 톰캣 내장되어 별도 설치 필요없음
+    spring-boot-starter-logging
+    - log 지원, 최신 실무는 롬복 Slf4j 사용
+    spring-starter-test
+    - junit 테스트 라이브러리
+    - Mock, AssertJ 지원
+ */
 @SpringBootApplication
 public class JpashopApplication {
 
