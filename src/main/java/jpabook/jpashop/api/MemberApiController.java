e4d4d90e8cc4d4541b4cb5eb521377421e5e8394
omea92 <thdjun92@naver.com>
2025-03-31 00:36:02 +0900

    API 개발 : 회원조회(호출, 응답 별도 DTO 생성)

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // DATA 바로 JSON 형식으로 반환
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) { //@Valid 효과로 값 유효성 체크
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    // 엔티티를 파라미터로 전달하여 외부에 노출시키면 절대안된다
+    // 엔티티를 타인이 변경하고 그대로 파라미터로 전달할 시 변경으로 인한 에러로 장애날수있어서다.
+    // 해결1. 엔티티는 변경가능성이 높기에 필드명 변경되면 API 에러날수있으니 별도 DTO 만들어 전달하자
+    // 해결2. API 호출/응답 각각 스펙에 맞는 별도 DTO(CreateMemberResponse, CreateMemberRequest)를 만들어 사용하자
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -11,6 +11,11 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
+/*
+    엔티티는 여러 개발자가 변경가능하게 접근된 객체로 API 개발 시 엔티티쪽이 변경되면 안좋다
+    - 결론: API 스펙에 맞춘 별도의 DTO를 만들어 활용해야 한다
+           엔티티를 파라미터에 셋팅해서 노출시키면 안된다.
+ */
 @Entity
 @Getter @Setter
 public class Member {
