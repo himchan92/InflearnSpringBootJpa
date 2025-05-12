df4c5786be79b510876647a1def8f0a922cfb926
omea92 <thdjun92@naver.com>
2025-05-12 23:46:13 +0900

    컨트롤러 뷰 기본 조회화면 실습

---
+++ src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello222"); //view 단으로 데이터 전달해주는 객체
+        return "hello"; //타임리프가 html 매핑지원해줘서 해당 파일명으로 template html 파일찾아 UI 보여주는것
+    }
+}
