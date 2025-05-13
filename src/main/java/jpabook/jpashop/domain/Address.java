da97feb34f53072d07d49051ba0af941d65657de
himchan92 <omea92@naver.com>
2025-05-13 12:58:06 +0900

    엔티티클래스 개발 실습(추후 JPA 기본편 개념 수강 후 복습필요)

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -2,10 +2,13 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
     private String city;
     private String street;
