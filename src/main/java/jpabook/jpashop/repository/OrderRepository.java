6e322116e7c2b60cd7c1d9d07cf9002214ae5938
omea92 <thdjun92@naver.com>
2025-06-09 22:38:14 +0900

    상품 검색 처리 실습(QueryDSL 방식 권장..)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,15 @@ public class OrderRepository {
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+            " where o.status = :status" +
+            " and m.name like :name", Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+            .setMaxResults(1000)
+            .getResultList();
     }
 }
