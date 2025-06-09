3491c8055fbd0819b063288a8d3f9a6a149d5871
omea92 <thdjun92@naver.com>
2025-06-09 21:55:08 +0900

    상품 Repository 실습

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {
+            em.persist(item); //없으면 신규등록
+        } else {
+            em.merge(item); //갱신
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
