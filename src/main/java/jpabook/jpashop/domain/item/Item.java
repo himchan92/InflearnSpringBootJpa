a73966db1bfe0dd4d78b5beb8d7372f77ec9a21e
omea92 <thdjun92@naver.com>
2025-06-09 21:52:07 +0900

    커스텀 예외처리 생성 실습

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 비즈니스로직
+     * - 엔티티안에 작성하는 것 권장
+     */
+    //상품 재고수량 증가
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    //상품 재고수량 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        //수량 없으면 예외처리
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock; //남은수량 대입
+    }
 }
