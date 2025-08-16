127666612b326a664424bad80cdf9167b72955a8
user <thdjun92@naver.com>
2025-08-16 16:53:05 +0900

    부트JPA활용1 복습 - 주문서비스

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문할상품생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문처리생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //Cascade 효과로 한벊저장시 하위같이 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문취소대상 조회
+        Order order = orderRepository.findOne(orderId);
+        //조회대상 취소
+        order.cancel();
+
+        //JPA 장점이 기존 MYBATIS, JDBC는 변경작업 매번 SQL 날려줘야하지만 JPA는 변경로직만 짜주면 자동 변경감지로 SQL 수행지원
+    }
+
+    //검색
+}
