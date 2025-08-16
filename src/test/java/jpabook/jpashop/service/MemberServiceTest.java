9ba08dbe6206793a0a0966e61e668ab8c5acaedd
omea92 <thdjun92@naver.com>
2025-08-16 11:09:48 +0900

    실무활용1 복습 - 회원기능테스트 ing

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+  @Test
+  public void 회원가입() throws Exception {
+
+  }
+
+  @Test
+  public void 중복회원예외() throws Exception {
+
+  }
+}
