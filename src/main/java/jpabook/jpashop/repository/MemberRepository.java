41c4c4aeddcf1b274d1471d8c5bfa796d4504617
omea92 <thdjun92@naver.com>
2025-08-16 11:08:06 +0900

    실무활용1 복습 - 서비스 실습

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -4,13 +4,14 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-  @PersistenceContext
-  private EntityManager em;
+  private final EntityManager em;
 
   public void save(Member member) {
     em.persist(member); // DB COMMIT 시점에 DB 반영
