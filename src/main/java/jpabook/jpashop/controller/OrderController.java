93a4bd87774c08337c2ec7efe96098e455b9da16
omea92 <thdjun92@naver.com>
2025-08-17 13:46:39 +0900

    실무활용1 복습 - 주문검색/취소

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,5 +47,11 @@ public class OrderController {
     List<Order> orders = orderService.findOrders(orderSearch);
     model.addAttribute("orders", orders);
     return "order/orderList";
+  }
+
+  @PostMapping("order/{orderId}/cancel")
+  public String cancelOrder(@PathVariable("orderId") Long orderId) {
+    orderService.cancelOrder(orderId);
+    return "redirect:/orders";
   }
 }
