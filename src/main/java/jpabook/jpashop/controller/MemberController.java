48734169b64d09d35f5dcdb561afcb05ef7e92ea
omea92 <thdjun92@naver.com>
2025-08-17 13:05:48 +0900

    실무활용1 복습 - 회원가입/목록, 상품등록/목록

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,5 +18,15 @@ public class MemberController {
   public String createForm(Model model) {
     model.addAttribute("memberForm", new MemberForm());
     return "members/createMemberForm";
+  }
+
+  //엔티티를 그대로 반환금지 -> 모든 필드가 노출되어 보안위배됨
+  //별도로 노출할 필드만으로 DTO 따로 구성해서 파라미터로 넘겨준다.
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+
+    return "members/memberList";
   }
 }
