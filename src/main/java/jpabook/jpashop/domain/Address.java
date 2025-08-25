4fbea6b46d288e89d7a8512b5cf953d78e3da64f
himchan92 <omea92@naver.com>
2025-08-25 12:22:56 +0900

    야생형 JPA 실무1편 엔티티 실습복습

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,22 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //값타입 변경불가로 설계하기에 기본생성자 PROTECTED로 같은 범이내에서만 접근되게 설계권장
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
