a749151b0f3c55de27553e507d4ffe3ff4c3d882
omea92 <thdjun92@naver.com>
2025-11-03 22:19:45 +0900

    기본 controller 예제 주석

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") // GET 방식 url 매핑
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+        model.addAttribute("data", "hello!!"); // view 단 데이터 전달 객체
+        return "hello"; // view - templates - html 파일명 일치
     }
 }
