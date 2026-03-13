205ecbed6f975cd7eefbc952d7a8714ed3343090
himchan92 <omea92@naver.com>
2026-03-13 12:58:52 +0900

    주문 도메인

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+    @Test
+    public void 주문() throws Exception {
+        Member member = new Member();
+
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+
+    }
+}
